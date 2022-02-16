79782918b8744f3811c8b40667a7d910a37f87ea
yunea <code.experience01@gmail.com>
2022-02-16 17:53:36 +0100

    reussi

--- eau12.py
+++ eau12.py
@@ -8,33 +8,23 @@ def my_bubble_sort(array):
     i = (len(array)-1)
     j = 0
     x = 0
-    c = True
-    while i > 1:
-        c = True
-        print("i="+str(i))
-        print("j="+str(j))
-        while j < i:
-
-            print(array)
+    while i >= 1:
+        while j <= i-1:
             if (array[j] > array[j+1]):
-
                 x = array[j]
                 array[j] = array[j+1]
                 array[j+1] = x
-                c = False
             j = j+1
         i = i-1
-
-        print("c="+str(c))
-        if c == True:
-            return (array)
+        j = 0
+    return (array)
 
 
 if len(sys.argv) > 3:
     args = sys.argv
     args.pop(0)
-    print(args)
-    tab = my_bubble_sort(args)
-    print(tab)
+    res = my_bubble_sort(args)
+    res_str = [str(a) for a in res]
+    print(' '.join(res_str))
 else:
     print("Error")
